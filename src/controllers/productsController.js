ad590f0ba974ce120b2e8f28fe3afb498be96b85
Agustina-Gonzalez20 <ramepirane@gmail.com>
2024-03-15 10:56:55 -0300

    Agrego Parte2 ML7-CRUD

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+//1.Traigo todos los prouctos de mi base de datos
+let productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
 const controller = {
@@ -19,11 +19,32 @@ const controller = {
 
 	// Create - Form to create
 	create: (req, res) => {
-		res.render("product-create-form")
+	res.render('product-create-form')
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
+		//2. obtengo los datos q me manda el usuario por el body
+		const {name,price,discount,category,description} = req.body;
+		//3. Crea nueva id.
+		const nuevaId= products[products.length - 1].id + 1;
+		//4.Nuevo objeto producto.
+		const nuevoProducto= {
+		 id: nuevaId,
+		 name,
+		 price,
+		 discount,
+		 category: category =="visited"? "visited": "in-sale",
+		 description,
+		 image: "img-bicicleta-fierce.jpg",
+		}
+		//5.Insertar al nuevoProducto al array
+	    products = [...products,nuevoProducto];
+	    products = JSON.stringify(products,null,3);
+	 
+	   fs.writeFileSync(productsFilePath,products,'utf-8');
+ 
+	   return res.redirect("/products")
 	
 	},
 
@@ -35,13 +56,40 @@ const controller = {
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		res.send("producto editado")
+		const { id } = req.params;
+		const {name,price,discount,category,description} = req.body;
+ const productsMap = products.map((p) => {
+    if (p.id === +id) {
+      const productEditado = {
+        ...p,
+        name: name.trim(),
+        price: +price,
+		discount: discount.trim(),
+        description: description.trim(),
+		category: category =="visited"? "visited": "in-sale",
+      };
+
+      return productEditado;
+    }
+
+    return p;
+  });
+  products = JSON.stringify(productsMap,null,3);
+  fs.writeFileSync(productsFilePath,products,'utf-8');
+  res.redirect("/products");
+
+    
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		res.send("producto eliminado")
+		const {id} = req.params
 
+		const productLessOne = products.filter(p => p.id !== +id)
+	  
+		products = JSON.stringify(productLessOne,null,3);
+		fs.writeFileSync(productsFilePath,products,'utf-8');
+		res.redirect("/products");
 	}
 };
 
